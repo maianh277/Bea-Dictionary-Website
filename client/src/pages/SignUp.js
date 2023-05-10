693a0cc271db04ba23258c362fffdfc5610c90f3
hiimdu <dotranmaianh2002@gmail.com>
2023-05-10 16:51:10 +0700

    add register function

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import SignupForm from "../component/form/SignupForm";
 const SignUp = () => {
     return (
@@ -6,7 +5,7 @@ const SignUp = () => {
             <div>
                 <img
                     src="https://images.unsplash.com/photo-1476357471311-43c0db9fb2b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                    alt="SignUp picture"
+                    alt="SignUp"
                     className="w-full h-full"
                 />
             </div>
