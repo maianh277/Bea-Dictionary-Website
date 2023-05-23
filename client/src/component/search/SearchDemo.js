62126b27ccc0aed2276d6c84219d9d95562ca20d
hiimdu <dotranmaianh2002@gmail.com>
2023-05-24 02:03:06 +0700

    api

--- client/src/component/search/SearchDemo.js
+++ client/src/component/search/SearchDemo.js
@@ -1,62 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Word from "../../component/word/Word";
+
 const SearchDemo = () => {
-    const [word, setWord] = useState("");
-    const [meanings, setMeanings] = useState([]);
-    const [phonetics, setPhonetics] = useState([]);
-    const [partOfSpeech, setPartOfSpeech] = useState([]);
-    const [definition, setDefinition] = useState([]);
-    useEffect(() => {
-        if (word) {
-            axios
-                .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-                .then((res) => {
-                    setMeanings(res.data[0].meanings);
-                    setPhonetics(res.data[0].phonetics);
-                    setPartOfSpeech(res.data[0].partOfSpeech);
-                    setDefinition(res.data[0].definition);
-                })
-                .catch((err) => console.log(err));
-        }
-    }, [word]);
+  const [searchedWord, setSearchedWord] = useState("");
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [phonetics, setPhonetics] = useState([]);
+  const [partOfSpeech, setPartOfSpeech] = useState([]);
+  const [definition, setDefinition] = useState([]);
 
-    const handleChange = (e) => {
-        setWord(e.target.value);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setWord(searchedWord);
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`)
+      .then((res) => {
+        setMeanings(res.data[0].meanings);
+        setPhonetics(res.data[0].phonetics);
+        setPartOfSpeech(res.data[0].partOfSpeech);
+        setDefinition(res.data[0].definition);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setWord(e.target.word.value);
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit} className="flex my-2">
-                <label className="mx-2"></label>
-                <input
-                    name="word"
-                    onChange={handleChange}
-                    value={word}
-                    className="px-3 py-2 rounded-lg border border-blue-200 w-full"
-                ></input>
-                <button
-                    type="submit"
-                    className="mx-2 px-3 py-2 bg-blue-400 text-white rounded-lg"
-                >
-                    Search
-                </button>
-            </form>
-            {word && (
-                <Word
-                    word={word}
-                    meanings={meanings}
-                    phonetics={phonetics}
-                    partOfSpeech={partOfSpeech}
-                    definition={definition}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <form className="flex my-2">
+        <label className="mx-2"></label>
+        <input
+          name="word"
+          onChange={(e) => setSearchedWord(e.target.value)}
+          value={searchedWord}
+          className="px-3 py-2 rounded-lg border border-blue-200 w-full"
+        />
+        <button
+          type="submit"
+          className="mx-2 px-3 py-2 bg-blue-400 text-white rounded-lg"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+      </form>
+      {word && (
+        <Word
+          word={word}
+          meanings={meanings}
+          phonetics={phonetics}
+          partOfSpeech={partOfSpeech}
+          definition={definition}
+        />
+      )}
+    </div>
+  );
 };
+
 export default SearchDemo;
--- client/src/component/word/SynoAnno.js
+++ client/src/component/word/SynoAnno.js
@@ -2,43 +2,51 @@ import React from "react";
 import CommentBox from "../comment/CommentBox";
 
 const SynoAnno = (props) => {
-    const meanings = props.meanings || [];
+  const { meanings } = props;
 
-    const synonymsArray = [];
-    const antonymsArray = [];
+  const synonymsArray = [];
+  const antonymsArray = [];
 
-    meanings.forEach((item) => {
-        item.definitions.forEach((def) => {
-            if (def.synonyms) {
-                synonymsArray.push(def.synonyms);
-            }
-            if (def.antonyms) {
-                antonymsArray.push(def.antonyms);
-            }
-        });
+  meanings.forEach((item) => {
+    item.definitions.forEach((def) => {
+      if (def.synonyms) {
+        synonymsArray.push(def.synonyms);
+      }
+      if (def.antonyms) {
+        antonymsArray.push(def.antonyms);
+      }
     });
+  });
 
-    return (
-        <div className="flex justify-around mx-20 my-5 flex-col">
-            <div>
-                <div className="flex flex-row gap-3 mb-4">
-                    <div className="rounded-lg w-max h-8 p-1 bg-[#72CCFF] font-semibold text-center">
-                        Synonyms
-                    </div>
-                    <p>{synonymsArray.join(" ")}</p>
-                </div>
+  return (
+    <div className="flex justify-around mx-20 my-5 flex-col">
+      {!synonymsArray ? (
+        <div>
+          <div className="flex flex-row gap-3 mb-4">
+            <div className="rounded-lg w-max h-8 p-1 bg-[#72CCFF] font-semibold text-center">
+              Synonyms
             </div>
-            <div>
-                <div className="flex flex-row gap-3">
-                    <div className="rounded-lg w-max h-8 p-1 bg-[#FFF175] font-semibold text-center">
-                        Antonyms
-                    </div>
-                    <p>{antonymsArray.join(" ")}</p>
-                </div>
-            </div>
-            <CommentBox />
+            <p>{synonymsArray.join(" ")}</p>
+          </div>
         </div>
-    );
+      ) : (
+        ""
+      )}
+      {!antonymsArray ? (
+        <div>
+          <div className="flex flex-row gap-3">
+            <div className="rounded-lg w-max h-8 p-1 bg-[#FFF175] font-semibold text-center">
+              Antonyms
+            </div>
+            <p>{antonymsArray.join(" ")}</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <CommentBox />
+    </div>
+  );
 };
 
 export default SynoAnno;
